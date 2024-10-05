d5b1f3fc96984444fdd6815b5aed40d362141268
HeitorHrecek <heitorhrecek@outlook.com>
2024-10-05 01:33:44 -0300

    Adjustment in FolderService: correction of mapping and changeName method

--- src/main/java/com/example/lockly/serviceLayer/FolderService.java
+++ src/main/java/com/example/lockly/serviceLayer/FolderService.java
@@ -1,16 +1,48 @@
 package com.example.lockly.serviceLayer;
 
+import com.example.lockly.controllerLayer.dtos.FolderDto;
 import com.example.lockly.dataproviderLayer.FolderDataProvider;
+import com.example.lockly.mapper.FolderMapper;
 import com.example.lockly.domainLayer.Folder;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-@AllArgsConstructor
-public class FolderService {
-    private final FolderDataProvider dataProvider;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public Folder consultById(Integer id) {
-        return dataProvider.consultById(id);
+@Service
+public class FolderService {
+
+    private final FolderDataProvider dataProvider;
+    private final FolderMapper mapper;
+
+    @Autowired
+    public FolderService(FolderDataProvider dataProvider, FolderMapper mapper) {
+        this.dataProvider = dataProvider;
+        this.mapper = mapper;
+    }
+
+    public FolderDto register(FolderDto folderDto) {
+        Folder folder = mapper.forDomainFromDto(folderDto);
+        return mapper.forDto(dataProvider.save(folder));
+    }
+
+    public List<FolderDto> consultAllByUser(Long userId) {
+        return dataProvider.consultAllByUser(userId).stream()
+                .map(FolderMapper::forDto)
+                .collect(Collectors.toList());
+    }
+
+    public void delete(Long id) {
+        dataProvider.delete(id);
+    }
+
+    public FolderDto changeName(FolderDto folderDto, Long id) {
+        Folder folder = dataProvider.consultById(id).orElse(null);
+        if (folder != null) {
+            folder.setName(folderDto.name());
+            return mapper.forDto(dataProvider.save(folder));
+        }
+        return null;
     }
 }
