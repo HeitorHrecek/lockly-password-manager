264d97c165d17b8404d57852505204fe204cc6ff
HeitorHrecek <heitorhrecek@outlook.com>
2024-10-15 00:53:39 -0300

    ConsultAllFolderByUserErroException exception update

--- src/main/java/com/example/lockly/dataproviderLayer/exceptions/folder/ConsultAllFolderByUserErroException.java
+++ src/main/java/com/example/lockly/dataproviderLayer/exceptions/folder/ConsultAllFolderByUserErroException.java
@@ -1,6 +1,6 @@
 package com.example.lockly.dataproviderLayer.exceptions.folder;
 
-public class ConsultAllFolderByUserErroException extends RuntimeException {
+public class ConsultAllFolderByUserErroException extends java.lang.RuntimeException {
     public ConsultAllFolderByUserErroException(String message) {
         super(message);
     }
