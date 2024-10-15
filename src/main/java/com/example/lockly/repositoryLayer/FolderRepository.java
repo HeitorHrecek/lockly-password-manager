3bf36a69637bdf6ec28915c613ed8dd25febc87e
HeitorHrecek <heitorhrecek@outlook.com>
2024-10-15 07:23:50 -0300

    Refactor FolderRepository to use FolderEntity instead of domain, update return types for consultAllByUser.

--- src/main/java/com/example/lockly/repositoryLayer/FolderRepository.java
+++ src/main/java/com/example/lockly/repositoryLayer/FolderRepository.java
@@ -1,12 +1,12 @@
 package com.example.lockly.repositoryLayer;
 
-import com.example.lockly.domainLayer.Folder;
+import com.example.lockly.repositoryLayer.entities.FolderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface FolderRepository extends JpaRepository<Folder, Long> {
-    List<Folder> consultAllByUser(Long userId);
+public interface FolderRepository extends JpaRepository<FolderEntity, Long> {
+    List<FolderEntity> consultAllByUser(Long userId);
 }
