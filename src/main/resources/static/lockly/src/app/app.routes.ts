397e5474c4d44a6549da07d816f5945b29623d7b
VitorVieira <vitorhvvieira@gmail.com>
2024-10-18 13:47:29 -0300

    Mais algumas alterações

--- src/main/resources/static/lockly/src/app/app.routes.ts
+++ src/main/resources/static/lockly/src/app/app.routes.ts
@@ -4,9 +4,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-export const routes: Routes = [
-  { path: 'cadastro', component: CadastroComponent }
-];
+export const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
