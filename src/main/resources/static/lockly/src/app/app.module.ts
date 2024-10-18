637116f07639567bbab049e1190af8aa800b443e
VitorVieira <vitorhvvieira@gmail.com>
2024-10-18 13:46:49 -0300

    Arrumadno código que estava na branch errada

--- src/main/resources/static/lockly/src/app/app.module.ts
+++ src/main/resources/static/lockly/src/app/app.module.ts
@@ -3,13 +3,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { CadastroComponent } from './telas/cadastro/cadastro.component';
 import { AppRoutingModule } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent
   ],
   imports: [
     BrowserModule,
--- src/main/resources/static/lockly/src/app/app.routes.ts
+++ src/main/resources/static/lockly/src/app/app.routes.ts
@@ -2,7 +2,6 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CadastroComponent } from './telas/cadastro/cadastro.component';
 
 
 export const routes: Routes = [
