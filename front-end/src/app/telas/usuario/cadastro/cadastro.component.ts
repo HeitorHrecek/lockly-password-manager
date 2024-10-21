f18158a49bfd44c92c060733a951487cf995d675
VitorVieira <vitorhvvieira@gmail.com>
2024-10-21 16:36:56 -0300

    Resolvendo conflitos do merge da development

--- front-end/src/app/telas/usuario/cadastro/cadastro.component.ts
+++ front-end/src/app/telas/usuario/cadastro/cadastro.component.ts
@@ -4,7 +4,6 @@ import { CadastroUsuarioService } from '../cadastro.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro',
