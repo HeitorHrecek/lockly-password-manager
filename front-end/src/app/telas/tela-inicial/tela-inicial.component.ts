d5db0cccddd1ba77e606138a799f5bb3cc0a58f1
VitorVieira <vitorhvvieira@gmail.com>
2024-10-25 15:34:48 -0300

    Terminando alinhamento das sections e finalizando tela

--- front-end/src/app/telas/tela-inicial/tela-inicial.component.ts
+++ front-end/src/app/telas/tela-inicial/tela-inicial.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-inicial',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './tela-inicial.component.css'
 })
 export class TelaInicialComponent {
+  constructor(private router: Router) { }
 
+
+  entrar() {
+    this.router.navigate(['/login'])
+  }
+
+  cadastrar() {
+    this.router.navigate(['/cadastro'])
+  }
 }
